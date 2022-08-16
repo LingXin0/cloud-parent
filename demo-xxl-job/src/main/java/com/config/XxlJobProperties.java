5dd780af71fa608a08c136ad5ba223ae3c0e1ae4
dongxin <dongxinxin@cloudtogo.cn>
2022-08-16 17:54:45 +0800

    dongxin
    update

---
+++ demo-xxl-job/src/main/java/com/config/XxlJobProperties.java
@@ -0,0 +1,41 @@
+package com.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author DongXin
+ * @date 2022/8/16 10:43 上午
+ */
+@Data
+@ConfigurationProperties(prefix = "xxl.job")
+@Component
+public class XxlJobProperties {
+
+    private String adminAddresses;
+
+    private String accessToken;
+
+    private Exexutor exexutor;
+
+}
+
+@Data
+@ConfigurationProperties(prefix = "xxl.job.exexutor")
+@Configuration
+class Exexutor {
+
+    private String appName;
+
+    private String address;
+
+    private String ip;
+
+    private int port;
+
+    private String logPath;
+
+    private int logRetentionDays;
+}
